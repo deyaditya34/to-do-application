a08b74017cefe66db20f13cc79cbfe0189dd3bb4
Aditya Dey <deyaditya34@gmail.com>
2023-09-30 13:10:13 +0530

    updated

--- src/tasks/tasks.api.router.js
+++ src/tasks/tasks.api.router.js
@@ -10,9 +10,9 @@ const getTask = require("./get-task.api");
 const router = express.Router();
 
 router.post("/", createTask);
+router.get("/", searchTasks);
 router.get("/:id", getTask);
 router.delete("/:id", deleteTask);
 router.put("/:id", updateTask);
 router.put("/:id/:newStatus", taskStatus);
-router.get("/", searchTasks);
 module.exports = router;
