6c68e09395a2a38f737ebc3e507b2f5a7bedbc32
Aditya Dey <deyaditya34@gmail.com>
2023-09-30 13:14:06 +0530

    route updated

--- src/categories/delete-category.api.js
+++ src/categories/delete-category.api.js
@@ -3,7 +3,7 @@ const httpError = require("http-errors");
 const categoriesService = require("./categories.service");
 const buildApiHandler = require("../api-utils/build-api-handler");
 const userResolver = require("../middlewares/user-resolver");
-const paramsValidator = require("../middlewares/params-validator");
+
 
 async function controller(req, res) {
   const { id } = req.params;
@@ -29,11 +29,6 @@ async function validateParams(req, res, next) {
 
   next();
 }
-
-const missingParamsValidator = paramsValidator.createParamsValidator(
-  ["id"],
-  paramsValidator.PARAM_KEY.PARAMS
-);
 
 module.exports = buildApiHandler([
   userResolver,
--- src/categories/get-category.api.js
+++ src/categories/get-category.api.js
@@ -2,7 +2,6 @@ const httpError = require("http-errors");
 
 const buildApiHandler = require("../api-utils/build-api-handler");
 const userResolver = require("../middlewares/user-resolver");
-const paramsValidator = require("../middlewares/params-validator");
 const categoriesService = require("./categories.service");
 
 async function controller(req, res) {
@@ -20,11 +19,6 @@ async function controller(req, res) {
     });
   }
 }
-
-const missingParamsValidator = paramsValidator.createParamsValidator(
-  ["id"],
-  paramsValidator.PARAM_KEY.PARAMS
-);
 
 module.exports = buildApiHandler([
   userResolver,
